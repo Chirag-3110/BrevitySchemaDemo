33fa4c6d813ced9106feeff4f7b064a3eada1804
abhijan2402 <abhishek.jangid643@gmail.com>
2022-08-05 23:49:31 +0530

    correction in routes

--- src/Frontend/Home.js
+++ src/Frontend/Home.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 function Home() {
-  return (
+    return (
         <div className="App">
             <h1>Welcome to Home</h1>
-            <div style={{width:'50%',height:'40%',display:'flex',flexWrap:'wrap',justifyContent:"center"}}>
-                <button 
-                    style={{margin:10,width:200,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/user" style={{textDecoration:'none',color:"black"}}>User Part</Link>
+            <div style={{ width: '50%', height: '40%', display: 'flex', flexWrap: 'wrap', justifyContent: "center" }}>
+                <button
+                    style={{ margin: 10, width: 200, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/user" style={{ textDecoration: 'none', color: "black" }}>User Part</Link>
                 </button>
-                <button 
-                    style={{margin:10,width:200,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/order" style={{textDecoration:'none',color:"black"}}>Order Part</Link>
+                <button
+                    style={{ margin: 10, width: 200, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/order" style={{ textDecoration: 'none', color: "black" }}>Order Part</Link>
                 </button>
-                <button 
-                    style={{margin:10,width:200,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/ordertask" style={{textDecoration:'none',color:"black"}}>Order Task Part</Link>
+                <button
+                    style={{ margin: 10, width: 200, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/ordertask" style={{ textDecoration: 'none', color: "black" }}>Order Task Part</Link>
                 </button>
-                <button 
-                    style={{margin:10,width:200,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/comments" style={{textDecoration:'none',color:"black"}}>Notification Part</Link>
+                <button
+                    style={{ margin: 10, width: 200, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/notification" style={{ textDecoration: 'none', color: "black" }}>Notification Part</Link>
                 </button>
-                <button 
-                    style={{margin:10,width:200,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/notification" style={{textDecoration:'none',color:"black"}}>Comment Part</Link>
+                <button
+                    style={{ margin: 10, width: 200, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/comments" style={{ textDecoration: 'none', color: "black" }}>Comment Part</Link>
                 </button>
-                <button 
-                    style={{margin:10,width:200,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/workflow" style={{textDecoration:'none',color:"black"}}>Workflow Part</Link>
+                <button
+                    style={{ margin: 10, width: 200, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/workflow" style={{ textDecoration: 'none', color: "black" }}>Workflow Part</Link>
                 </button>
-                <button 
-                    style={{margin:10,width:400,height:50,backgroundColor:'#2AB2FF',color:'black',fontWeight:"bold",borderWidth:2,borderColor:"black",borderRadius:6}}>
-                    <Link to="/workflowdefinition" style={{textDecoration:'none',color:"black"}}>WorkflowDefinition Part</Link>
+                <button
+                    style={{ margin: 10, width: 400, height: 50, backgroundColor: '#2AB2FF', color: 'black', fontWeight: "bold", borderWidth: 2, borderColor: "black", borderRadius: 6 }}>
+                    <Link to="/workflowdefinition" style={{ textDecoration: 'none', color: "black" }}>WorkflowDefinition Part</Link>
                 </button>
             </div>
         </div>
-  );
+    );
 }
 
 export default Home;
